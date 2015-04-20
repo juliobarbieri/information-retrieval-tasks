90dac840ec370e1f0c314946f6fa58ee2e922267
Julio <juliobarbieri@yahoo.com.br>
2015-04-20 20:21:29 -0300

    Aproveitando a base do modelo vetorial para criação do indexador Lucene

--- indexer.py
+++ indexer.py
@@ -1,106 +1,99 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 30 2015
+Created on Tue Mar 24 2015
 @author: Julio Barbieri
 """
 
-import numpy as np
 import nltk
 import logging
-import re
-import pickle
 import util
 import time
-from tfidf import TfIdf
+import sys
+import lucene
 from util import file_exists
 from util import exit_error
 from util import setup_logger
 from util import get_values
-from util import valida_termo
-from engine import Engine
-from vector_space_model import VectorSpaceModel
+from util import verify_stemmer
+from util import format_text
 from xml.etree.ElementTree import ElementTree
+from nltk.stem.porter import PorterStemmer
 
-def leia(filename):
+from java.io import File
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.document import Document, Field
+from org.apache.lucene.index import IndexWriter, IndexWriterConfig
+from org.apache.lucene.store import SimpleFSDirectory
+from org.apache.lucene.util import Version
+
+def leia(filename, abstract_list):
 	logger =  setup_logger(util.NAME_INDEXER_LOGGER, util.INDEXER_LOG)
 	
 	if not file_exists(filename):
 		logger.error(util.FILE_NOT_FOUND % filename)
 		exit_error(util.EXITED_WITH_ERROR)
 	
-	indexes = {}
+	xml = ElementTree().parse(filename)
+	
+	qntd_dados = 0
 	
 	start_time = time.time()
 	
-	with open(filename) as fp:
-		lidos = 0
-		aproveitados = 0
-		for line in fp:
-			termo, documentos = trata_lista(line)
-			lidos = lidos + 1
-			if termo != None:
-				indexes[termo] = documentos
-				aproveitados = aproveitados + 1
-				
-	logger.debug(util.WORDS_TIME % (time.time() - start_time))
+	for i, record in enumerate(xml.findall('RECORD')):
+		key = 0
+		text = ''
+		for j, recordnum in enumerate(record.findall('RECORDNUM')):
+			key = recordnum.text
+
+		for j, abstract in enumerate(record.findall('ABSTRACT')):
+			text = abstract.text
+		
+		if text == '':
+			for j, extract in enumerate(record.findall('EXTRACT')):
+				text = extract.text
+			
+		qntd_dados = qntd_dados + 1
+		abstract_list[key] = util.format_text(text)
 	
-	logger.debug(util.LINES_READED_USED_FILE % (lidos, aproveitados, filename))
+	logger.debug(util.DOCUMENTS_TIME % (time.time() - start_time))
 	
-	unique_ids = []
-	unique_terms = []
+	logger.debug(util.TUPLES_READED_FILE % (qntd_dados, filename))
 	
-	for i in indexes:
-		unique_ids.extend(indexes[i])
-		unique_terms.append(i)
-	
-	start_time = time.time()
-	
-	struct = VectorSpaceModel(list(set(unique_ids)), unique_terms)
-	struct.setup_matrix(indexes)
-	
-	engine = Engine(TfIdf(), struct)
-	struct = engine.index()
-	
-	logger.debug(util.INDEX_TIME % (time.time() - start_time))
-	
-	return struct
-	
-def escreva(filename, struct):
+def index(abstract_list):
 	logger =  setup_logger(util.NAME_INDEXER_LOGGER, util.INDEXER_LOG)
 	
-	if filename == '':
-		logger.error(util.NO_FILE_SPECIFIED)
-		exit_error(util.EXITED_WITH_ERROR)
+	lucene.initVM()
+	indexDir = SimpleFSDirectory(File("index/"))
+	writerConfig = IndexWriterConfig(Version.LUCENE_4_9, StandardAnalyzer(Version.LUCENE_4_9))
+	writer = IndexWriter(indexDir, writerConfig)
 	
-	logger.debug(util.SAVING_STRUCTURE % filename)
-	
-	afile = open(filename, 'wb')
-	pickle.dump(struct, afile)
-	afile.close()
-	
-def trata_lista(line):
-	line = line[:-1]
-	vet_line = line.split(util.CSV_SEPARATOR)
-	
-	termo = vet_line[0]
-	documentos = vet_line[1]
-	
-	documentos = documentos[:-1]
-	documentos = documentos[1:]
-	
-	vet_docs = documentos.split(',')
-	vet_docs = list(map(int, vet_docs)) #[int(i) for i in vet_docs]
-	
-	if (valida_termo(termo)):
-		return (termo, vet_docs)
-	else:
-		return None, None
+	for key in abstract_list:
+		stemmer = PorterStemmer()
+		stopwords = set(nltk.corpus.stopwords.words('english'))
+		words = nltk.word_tokenize(abstract_list[key])
+		words = [stemmer.stem(word).upper() for word in words if word not in stopwords]
+		prepared_document = ' '.join(words)
+		
+		doc = Document()
+		
+		doc.add(Field("key", key, Field.Store.YES, Field.Index.ANALYZED))
+		doc.add(Field("content", prepared_document, Field.Store.YES, Field.Index.ANALYZED))
+		
+		writer.addDocument(doc)
+		
+	#for n, l in enumerate(sys.stdin):
+	#	doc = Document()
+	#	doc.add(Field("text", l, Field.Store.YES, Field.Index.ANALYZED))
+	#	writer.addDocument(doc)
+	#print "Indexed %d lines from stdin (%d docs in index)" % (n, writer.numDocs())
+	#print "Closing index of %d docs..." % writer.numDocs()
+	writer.close()
 	
 def parse_command_file():
 	logger =  setup_logger(util.NAME_INDEXER_LOGGER, util.INDEXER_LOG)
 	
-	struct = None
+	abstract_list = {}
 	
 	config_file = util.INDEXER_FILENAME
 	
@@ -113,12 +106,10 @@ def parse_command_file():
 	with open(config_file) as fp:
 		count = 0
 		for line in fp:
-			next_cmd, filename = get_values(line, count, util.CONFIG_SEPARATOR, util.NAME_INDEXER_LOGGER, util.INDEXER_LOG)
+			next_cmd, filename = get_values(line, count, util.CONFIG_SEPARATOR, util.NAME_IIG_LOGGER, util.II_GENERATOR_LOG)
 			
-			if next_cmd == util.CMD_LEIA and count == 0:
-				struct = leia(filename)
-			elif next_cmd == util.CMD_ESCREVA and count == 1:
-				escreva(filename, struct)
+			if next_cmd == util.CMD_LEIA:
+				leia(filename, abstract_list)
 			else:
 				logger.error(util.NE_IO_INSTRUCTION_ERROR % (count + 1))
 				exit_error(util.EXITED_WITH_ERROR)
@@ -126,6 +117,8 @@ def parse_command_file():
 			
 	logger.debug(util.LINES_READED_CONFIG % count)
 	logger.debug(util.CONFIG_END_PROCESSING % config_file)
+	
+	index(abstract_list)
 
 if __name__ == "__main__":
 	parse_command_file()
--- main.py
+++ main.py
@@ -5,14 +5,12 @@ Created on Sat Apr 4 2015
 @author: Julio Barbieri
 """
 
-import inverted_index_generator
 import indexer
 import query_processor
 import searcher
 import evaluator
 
 if __name__ == "__main__":
-	inverted_index_generator.parse_command_file()
 	indexer.parse_command_file()
 	query_processor.parse_command_file()
 	searcher.parse_command_file()
